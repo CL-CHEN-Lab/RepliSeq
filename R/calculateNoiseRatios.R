b55cdb73b2e98ac06f44252cc3be46db08170ecd
SamiLhll <elhilali.sami@gmail.com>
2021-03-09 16:29:23 +0100

    Adding a second method of calculating noise ratios to select the first peak for particular case where the second peak is higher than the first

--- R/calculateNoiseRatios.R
+++ R/calculateNoiseRatios.R
@@ -3,13 +3,13 @@
 #'
 #' @param rs_assay a Repli-seq assay (data.frame) loaded with readRS() and formatted as chr,start,stop,S1,...,Sx
 #' @param rs_control a Repli-seq control assay (data.frame) loaded with readRS() and formatted as chr,start,stop,S0
-#'
+#' @param noise_method either "max_density"(default) or "min_downslope"
 #' @return an array with the ratios in the order of rs_assay fractions
 #' @export
 #'
 
 
-calculateNoiseRatios <- function(rs_assay, rs_control) {
+calculateNoiseRatios <- function(rs_assay, rs_control, noise_method = "max_density") {
   # select control and fractions :
   temp_fractions <- rs_assay[,!(names(rs_assay) %in% c("chr","start","stop"))]
   temp_control <- rs_control[,!(names(rs_control) %in% c("chr","start","stop"))]
@@ -18,8 +18,14 @@ calculateNoiseRatios <- function(rs_assay, rs_control) {
   for (fraction in names(temp_fractions)) {
     temp_fraction_vs_control <- round(rs_assay[,fraction] / temp_control, digits = 3)
     temp_density <- density(temp_fraction_vs_control,na.rm = TRUE)
-    temp_max <- which.max(temp_density$y)
-    temp_ratio <- round(temp_density$x[temp_max],digits = 3)
+    if (noise_method == "min_downslope") {
+      temp_downslopes <- which(diff(temp_density$y) < 0)
+      temp_ratio <- temp_density$x[min(temp_downslopes)]
+    }
+    else if (noise_method == "max_density") {
+      temp_max <- which.max(temp_density$y)
+      temp_ratio <- round(temp_density$x[temp_max],digits = 3)
+    }
     to_return <- c(to_return,temp_ratio)
   }
   return(to_return)
